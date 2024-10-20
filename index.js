4b3bc1b0e01a03c2f7a3931e90e25e9e00746f12
Rajan Kumar <rajan.kumar0007028@gmail.com>
2024-10-20 17:25:16 +0530

    isAbove function is added

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 console.log("Learning Git");
 
-const name = "Rajan kumar"
+const name1 = "Rajan kumar";
 const DOB = "07/08/1998";
+const age = 26;
+
+function sayName() {
+  console.log("my name is ", name1);
+}
+sayName();
+
+function isAbove() {
+  return age >= 18;
+}
+
+console.log(isAbove());
